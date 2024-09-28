f7946f7e8b989927f713cb9c040ef215c667f435
MichelPastrana <michelpach13@hotmail.com>
2024-09-27 21:58:50 -0600

    Initial config for location route

--- src/locations/locations.service.ts
+++ src/locations/locations.service.ts
@@ -1,26 +1,42 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateLocationDto } from './dto/create-location.dto';
 import { UpdateLocationDto } from './dto/update-location.dto';
+import { Repository } from 'typeorm';
+import { Location } from './entities/location.entity';
 
 @Injectable()
 export class LocationsService {
+  constructor(
+    private locationRespository: Repository<Location>
+  ){}
+
   create(createLocationDto: CreateLocationDto) {
-    return 'This action adds a new location';
+    return this.locationRespository.save(createLocationDto);
   }
 
   findAll() {
-    return `This action returns all locations`;
+    return this.locationRespository.find();
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} location`;
+    const location = this.locationRespository.findOneBy({
+      locationId: id,
+    })
+    if (!location) throw new NotFoundException("Location not found")
+    return location;
   }
 
   update(id: number, updateLocationDto: UpdateLocationDto) {
-    return `This action updates a #${id} location`;
+    const location = this.locationRespository.preload({
+      locationId: id,
+      ...updateLocationDto,
+    })
+    return location;
   }
 
   remove(id: number) {
-    return `This action removes a #${id} location`;
+    return this.locationRespository.delete({
+      locationId: id,
+    })
   }
 }
