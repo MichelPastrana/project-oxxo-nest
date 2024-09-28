b8b7c779a6fe412a96e8bc5493a1ab56e4b329df
MichelPastrana <michelpach13@hotmail.com>
2024-09-27 21:05:22 -0600

    Better get methods

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -31,7 +31,11 @@ export class ProductsService {
   }
 
   findByProvider(id: string) {
-    return "ok";
+    return this.productRepository.findBy({
+      provider: {
+        providerId: id,
+      }
+    })
   }
 
   async update(id: string, updateProductDto: UpdateProductDto) {
