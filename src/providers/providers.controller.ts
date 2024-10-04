e52384606326e39393c6ab41535f01a533f70ced
MichelPastrana <michelpach13@hotmail.com>
2024-10-04 10:13:02 -0600

    User decorator for routes

--- src/providers/providers.controller.ts
+++ src/providers/providers.controller.ts
@@ -1,21 +1,24 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, NotFoundException, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, NotFoundException, UseGuards, UnauthorizedException } from '@nestjs/common';
 import { ProvidersService } from './providers.service';
 import { CreateProviderDto } from './dto/create-provider.dto';
 import { UpdateProviderDto } from './dto/update-provider.dto';
 import { AuthGuard } from 'src/auth/guards/auth.guard';
+import { User } from 'src/auth/entities/user.entity';
+import { UserData } from 'src/auth/decorators/user.decorator';
 
+@UseGuards(AuthGuard)
 @Controller('providers')
 export class ProvidersController {
   constructor(private readonly providersService: ProvidersService) {}
 
-  @UseGuards(AuthGuard)
   @Post()
   create(@Body() createProviderDto: CreateProviderDto) {
     return this.providersService.create(createProviderDto);
   }
 
   @Get()
-  findAll() {
+  findAll(@UserData() user: User) {
+    if (user.userRoles.includes('Employee')) throw new UnauthorizedException("Solo admins y manager autorizados");
     return this.providersService.findAll();
   }
 
