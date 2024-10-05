269402b3aad6c7dfbfab213d2c8fd7e102c5dc0b
MichelPastrana <michelpach13@hotmail.com>
2024-10-04 19:17:51 -0600

    Auth for routes

--- src/providers/providers.controller.ts
+++ src/providers/providers.controller.ts
@@ -5,39 +5,45 @@ import { UpdateProviderDto } from './dto/update-provider.dto';
 import { User } from 'src/auth/entities/user.entity';
 import { UserData } from 'src/auth/decorators/user.decorator';
 import { Auth } from 'src/auth/decorators/auth.decorator';
+import { ROLES } from 'src/auth/constants/roles.constants';
 
 @Controller('providers')
 export class ProvidersController {
   constructor(private readonly providersService: ProvidersService) {}
 
+  @Auth(ROLES.MANAGER)
   @Post()
   create(@Body() createProviderDto: CreateProviderDto) {
     return this.providersService.create(createProviderDto);
   }
 
-  @Auth('Employee')
+  @Auth(ROLES.EMPLOYEE, ROLES.MANAGER)
   @Get()
   findAll(@UserData() user: User) {
     if (user.userRoles.includes('Employee')) throw new UnauthorizedException("Solo admins y manager autorizados");
     return this.providersService.findAll();
   }
 
+  @Auth(ROLES.EMPLOYEE, ROLES.MANAGER)
   @Get(':id')
   findOne(@Param('id') id: string) {
     const provider = this.providersService.findOne(id);
     return provider;
   }
 
+  @Auth(ROLES.EMPLOYEE, ROLES.MANAGER)
   @Get('/name/:name')
   findByName(@Param('name') name: string){
     return this.providersService.findOneByName(name);
   }
 
+  @Auth(ROLES.MANAGER)
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateProviderDto: UpdateProviderDto) {
     return this.providersService.update(id, updateProviderDto);
   }
 
+  @Auth(ROLES.MANAGER)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.providersService.remove(id); 
