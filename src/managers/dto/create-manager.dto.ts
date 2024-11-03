f78701caf9e19e1b7fe0279d49d83d994487f6bb
MichelPastrana <michelpach13@hotmail.com>
2024-11-02 20:25:52 -0600

    feat: Manager changes for location

--- src/managers/dto/create-manager.dto.ts
+++ src/managers/dto/create-manager.dto.ts
@@ -1,30 +1,24 @@
-import { IsEmail, IsNumber, IsObject, IsOptional, IsString, MaxLength } from "class-validator";
+import { IsEmail, IsNumber, IsOptional, IsString, MaxLength } from "class-validator";
 import { Manager } from "../entities/manager.entity";
 import { Location } from "src/locations/entities/location.entity";
-import { ApiProperty, ApiPropertyOptional } from "@nestjs/swagger";
 
 export class CreateManagerDto extends Manager {
-    @ApiProperty()
     @IsString()
     @MaxLength(80)
     managerFullName: string;
 
-    @ApiProperty()
     @IsString()
     @IsEmail()
     managerEmail: string;
     
-    @ApiProperty()
     @IsNumber()
     managerSalary: number;
     
-    @ApiProperty()
     @IsString()
     @MaxLength(16)
     managerPhoneNumber: string;
     
-    @ApiPropertyOptional()
-    @IsObject()
+    @IsNumber()
     @IsOptional()
     location: Location;
 }
